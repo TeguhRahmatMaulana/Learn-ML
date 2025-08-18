7d30ab6df87030c7888ab1ecbcd2a1dad937fbdc
Teguh Rahmat Maulana <rahmattteguhmaulana@gmail.com>
2025-08-18 22:15:06 +0700

    Add files via upload

---
+++ Exercise/Exercise 1.py
@@ -0,0 +1,26 @@
+# Tugas 
+
+# Menkonversi  farenheit ke kelvin 
+# Menkonversi kelvin ke farenheit
+
+
+# Farenheit ke kelvin
+
+print("==Konversi Farenheit ke Kelvin==")
+
+farenheit = float(input("Masukkan suhu dalam farenheit :"))
+
+kelvin = (5 / 9) * (farenheit - 32) + 273.15
+
+print("suhu dalam kelvin adalah :", kelvin, "kelvin") 
+
+
+# Kelvin ke Farenheit
+
+print("==Konversi Farenheit ke Kelvin==")
+
+kelvin = float(input("Masukkan suhu dalam kelvin :"))
+
+farenheit = (9/5) * (kelvin - 273.15) + 32
+
+print("suhu dalam farenheit adalah :", farenheit, "farenheit") 
